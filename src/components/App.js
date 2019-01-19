6e317ed958d89d6be6bbb30eaa9cd18b391c4073
Kai Chang <kaijchang@gmail.com>
2019-01-19 09:37:10 -0800

    basic layout

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Container>
+        <h1>CardinalHours</h1>
+        <Row>
+          <Col></Col>
+          <Col></Col>
+        </Row>
+      </Container>
+    )
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import App from './components/App';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Since we are using HtmlWebpackPlugin WITHOUT a template, we should create our own root node in the body element before rendering into it
+let root = document.createElement('div');
+
+root.id = 'root';
+document.body.appendChild(root);
+
+// Now we can render our application into it
+ReactDom.render(<App />, document.getElementById('root'));
