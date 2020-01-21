ec62e6339cd578d643d64a4019a0a0156ae69028
Rishi Nath <rishi.nath@gmail.com>
2020-01-20 21:16:03 -0800

    add/remove sessions WARNING, BLEEDING EDGE

---
+++ src/components/admin/SessionsConfig.js
@@ -0,0 +1,122 @@
+import React, { Component } from "react";
+import moment from "moment";
+import {
+    Button,
+    Input,
+    InputGroup,
+    InputGroupAddon,
+    InputGroupText
+} from 'reactstrap';
+
+const edge_format = 'MMM Do, YYYY HH:mm a';
+
+export default class SessionsConfig extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            is_showing: false,
+            is_adding: false,
+
+            date: '',
+            hours: '',
+        };
+
+        this.handleToggle = this.handleToggle.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleAddSession = this.handleAddSession.bind(this);
+    }
+
+    handleAddSession() {
+        if (moment(this.state.date).isValid()) {
+             this.props.handleAddSession(moment(this.state.date).toISOString(),
+                                         moment(this.state.date).add(this.state.hours, 'hours').toISOString());
+
+             this.setState({
+                 date: '',
+                 hours: ''
+             })
+        }
+    }
+
+    handleToggle(event) {
+        this.setState({
+            [event.target.name]: !this.state[event.target.name]
+        });
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    render() {
+        return <React.Fragment>
+            <Button name='is_showing'
+                    onClick={this.handleToggle}>Edit Sessions</Button>
+            {
+                this.state.is_showing ?
+                    <div>
+                        <div>
+                            <Button size='sm'
+                                    name='is_adding'
+                                    onClick={this.handleToggle}>Add/Remove Sessions</Button>
+                            {
+                                this.state.is_adding ?
+                                    <div>
+                                        <InputGroup>
+                                            <InputGroupAddon addonType='prepend'>
+                                                <InputGroupText>Date of Session</InputGroupText>
+                                            </InputGroupAddon>
+                                            <Input
+                                                name='date'
+                                                value={this.state.date}
+                                                onChange={this.handleChange}
+                                            />
+                                        </InputGroup>
+                                        <InputGroup>
+                                            <InputGroupAddon addonType='prepend'>
+                                                <InputGroupText># of Hours</InputGroupText>
+                                            </InputGroupAddon>
+                                            <Input
+                                                type='number'
+                                                name='hours'
+                                                value={this.state.hours}
+                                                onChange={this.handleChange}
+                                            />
+                                        </InputGroup>
+                                        <Button outline
+                                                color={ this.state.date === '' || moment(this.state.date).isValid() ? 'success' : 'danger' }
+                                                size='sm'
+                                                onClick={this.handleAddSession}>
+                                            Add This Session
+                                        </Button>
+                                    </div> :
+                                    <div>
+                                        {
+                                            this.props.sessions.map((session, idx) =>
+                                                <div key={idx}>
+                                                    {
+                                                        moment(session.start).format(edge_format) + " - " + moment(session.end).format(edge_format) + ": " +
+                                                        moment(session.end).diff(moment(session.start), 'hours', true).toFixed(2) + " "
+                                                    }
+                                                    <Button outline
+                                                            size='sm'
+                                                            color='danger'
+                                                            onClick={e => this.props.handleRemoveSession(session)}>
+                                                        Remove Session
+                                                    </Button>
+                                                </div>
+                                            )
+                                        }
+                                    </div>
+                            }
+
+                        </div>
+                    </div> : null
+            }
+
+        </React.Fragment>
+    }
+}
--- src/components/admin/UserConfig.js
+++ src/components/admin/UserConfig.js
@@ -10,7 +10,8 @@ import {
 
 import log from 'electron-log';
 
-import UserStore from '../../state/UserStore'
+import SessionsConfig from './SessionsConfig';
+
 import DB from '../../state/DB';
 
 export default class UserConfig extends Component {
@@ -21,41 +22,50 @@ export default class UserConfig extends Component {
 
         this.state = {
             users: user_names,
-            search_value: '',
-            selected_user_file_name: null,
-            selected_user_name: null,
-            selected_user_id: null,
+            search: '',
+
+            user_original_name: null,
+            user_name: null,
+            user_id: null,
+            user_sessions: null,
         };
 
         this.handleSelectUser = this.handleSelectUser.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.handleDropUser = this.handleDropUser.bind(this);
         this.handleSaveUser = this.handleSaveUser.bind(this);
+
+        this.handleRemoveSession = this.handleRemoveSession.bind(this);
+        this.handleAddSession = this.handleAddSession.bind(this);
     }
 
     handleSaveUser() {
         let user = DB.query({
-            name: this.state.selected_user_file_name
+            name: this.state.user_original_name
         });
 
-        user.name = this.state.selected_user_name;
-        user.id = this.state.selected_user_id;
+        user.name = this.state.user_name;
+        user.id = this.state.user_id;
 
-        DB.updateUser(this.state.selected_user_file_name, user);
+        DB.updateUser(this.state.user_original_name, user);
     }
 
     handleDropUser() {
-        let users = this.state.users.filter(user => user !== this.state.selected_user_file_name);
+        let user_names = this.state.users.filter(user => user !== this.state.user_original_name);
 
         let dropped_user = DB.query({
-            name: this.state.selected_user_file_name
+            name: this.state.user_original_name
         });
 
         this.setState({
-            users: users,
-            selected_user_file_name: null,
-            selected_user_name: null,
-            selected_user_id: null,
+            users: user_names,
+
+            user_original_name: null,
+            user_name: null,
+            user_id: null,
+            user_sessions: null,
+
+            sessions_opened: false,
         });
 
         DB.dropUser(dropped_user);
@@ -74,13 +84,38 @@ export default class UserConfig extends Component {
 
         if (user) {
             this.setState({
-                selected_user_file_name: user.name,
-                selected_user_name: user.name,
-                selected_user_id: user.id
+                user_original_name: user.name,
+                user_name: user.name,
+                user_id: user.id,
+                user_sessions: user.sessions,
+                sessions_opened: false,
             });
         } else {
             log.error('User not found:' + name);
         }
+    }
+
+    handleRemoveSession(removed_session) {
+        let updated_sessions = this.state.user_sessions;
+        updated_sessions.splice(this.state.user_sessions.findIndex(session => session.end === removed_session.end), 1);
+
+        this.setState({
+            user_sessions: updated_sessions
+        });
+    }
+
+    handleAddSession(start, end) {
+        const session = {
+            start: start,
+            end: end
+        };
+
+        let updated_sessions = this.state.user_sessions;
+        updated_sessions.push(session);
+
+        this.setState({
+            user_sessions: updated_sessions
+        })
     }
 
     render() {
@@ -97,28 +132,35 @@ export default class UserConfig extends Component {
                 />
                 <br/>
                 {
-                    this.state.selected_user_file_name ?
+                    this.state.user_original_name ?
                         <div>
                             <InputGroup>
                                 <InputGroupAddon
                                     addonType='prepend'><InputGroupText>Name</InputGroupText></InputGroupAddon>
-                                <Input value={this.state.selected_user_name}
-                                       name='selected_user_name'
+                                <Input value={this.state.user_name}
+                                       name='user_name'
                                        onChange={this.handleChange}/>
                             </InputGroup>
                             <br/>
                             <InputGroup>
                                 <InputGroupAddon
                                     addonType='prepend'><InputGroupText>ID</InputGroupText></InputGroupAddon>
-                                <Input value={this.state.selected_user_id}
-                                       name='selected_user_id'
+                                <Input value={this.state.user_id}
+                                       name='user_id'
                                        onChange={this.handleChange}/>
                             </InputGroup>
                             <br/>
+                            <SessionsConfig
+                                sessions={this.state.user_sessions}
+                                handleRemoveSession={this.handleRemoveSession}
+                                handleAddSession={this.handleAddSession}
+                            />
+                            <br/>
                             <Button color='success'
-                                    onClick={this.handleSaveUser}>{`Save Changes to ${this.state.selected_user_name}`}</Button>
+                                    onClick={this.handleSaveUser}>{`Save Changes to ${this.state.user_name}`}</Button>
                             {' '}
                             <Button color='danger' onClick={this.handleDropUser}>Drop User</Button>
+                            <br/>
                         </div> : null
                 }
                 <br/>
